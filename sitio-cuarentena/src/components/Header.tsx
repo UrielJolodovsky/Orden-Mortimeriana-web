3458c3758e3661943a7ccf55cabf596e0c8f3367
Uriel Jolodovsky <46960341@est.ort.edu.ar>
2023-10-19 12:57:24 -0300

    Header started

--- sitio-cuarentena/src/components/Header.tsx
+++ sitio-cuarentena/src/components/Header.tsx
@@ -1,9 +1,16 @@
 
-
 export default function Header() {
     return (
         <div>
-            <h1>Header</h1>
+            <nav id="header" className="">
+                <a href='#header' className=""><img></img></a>
+                <ul>
+                    <li><a href="#quienes-somos">Quienes somos</a></li>
+                    <li><a href="#noticias">Noticias</a></li>
+                    <li><a href="#competencias-anteriores" className=''>Competencias anteriores</a></li>
+                    <li><a href="#contacto">Contacto</a></li>
+                </ul>
+            </nav>
         </div>
     )
 }
