4d2490e29d02368954b84b778767f64e85251d0c
Uriel Jolodosvky <46960341@est.ort.edu.ar>
2023-10-20 13:38:27 -0300

    minor changes

--- sitio-cuarentena/src/components/Header.tsx
+++ sitio-cuarentena/src/components/Header.tsx
@@ -1,9 +1,12 @@
+import Link from "next/link";
 
 export default function Header() {
+
+
     return (
         <div>
             <nav id="header" className="">
-                <a href='#header' className=""><img></img></a>
+                <Link href='#header' className=""><img></img></Link>
                 <ul>
                     <li><a href="#quienes-somos">Quienes somos</a></li>
                     <li><a href="#noticias">Noticias</a></li>
