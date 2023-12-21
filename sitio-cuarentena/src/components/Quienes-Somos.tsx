836e95962385d9b224504f30b4571bdbdc607de1
Uriel Jolodovsky <46960341@est.ort.edu.ar>
2023-12-21 13:22:55 -0300

    "Competencias Anteriores" table responsive

--- sitio-cuarentena/src/components/Quienes-Somos.tsx
+++ sitio-cuarentena/src/components/Quienes-Somos.tsx
@@ -1,4 +1,10 @@
-import CardNotice from "./CardNotice";
+import React, { useState } from 'react'
+import { Swiper, SwiperSlide } from "swiper/react"
+import "swiper/css"
+import "swiper/css/effect-coverflow"
+import EffectCoverflow  from "swiper"
+import { Autoplay } from 'swiper/modules'
+import CardNotice from './CardNotice'
 
 
 export default function QuienesSomos() {
@@ -24,7 +30,7 @@ export default function QuienesSomos() {
                     <h1 className="text-center pt-5 pb-5 text-3xl leading-10 font-semibold">Noticias</h1>
                     <div className="flex flex-wrap justify-center items-center mx-10 mb-10 gap-10 relative">
                         {noticias.map((noticia) => (
-                            CardNotice(noticia)
+                                <CardNotice fecha={noticia.fecha} title={noticia.title} noticia={noticia.noticia} />
                         )
                         )}
                     </div>
@@ -32,21 +38,6 @@ export default function QuienesSomos() {
                 <hr className="border-black"></hr>
                 <div id="competencias-anteriores" className="mx-3 flex flex-col justify-center items-center pb-5">
                     <h1 className="text-center pt-5 pb-10 text-3xl leading-10 font-semibold">Competencias anteriores</h1>
-                    {/* Table with results of all olympiads competitions */}
-                    {/* <table className="bg-[#564C4A] m-1 text-center text-white rounded-2xl">
-                        <tbody>
-                            <tr className="">
-                                <td className="w-56"><b>Competencia</b></td>
-                                <td className="w-56"><b>Enunciados</b></td>
-                                <td className="w-56"><b>Resultados</b></td>
-                            </tr>
-                            <tr className="p-1">
-                                <td><p>CUARENTENA (2020)</p></td>
-                                <td><a href="">Enunciados</a></td>
-                                <td><a href="">Resultados</a></td>
-                            </tr>
-                        </tbody>
-                    </table> */}
                     <main className="text-center bg-white rounded-[15px] text-black">
                         <section>
                             {/* Header table */}
@@ -55,7 +46,7 @@ export default function QuienesSomos() {
                             {/* Body table */}
                             <table className="m-2">
                                 <thead className="h-10">
-                                    <tr className="text-xl">
+                                    <tr className="sm:text-xl text-base">
                                         <th className="p-1">Competencia</th>
                                         <th className="w-32 p-1">Enunciados</th>
                                         <th className="p-2">Resultados</th>
@@ -64,32 +55,32 @@ export default function QuienesSomos() {
                                 <tbody className="p-2 m-2">
                                     <tr className="m-3 h-12">
                                         <td className="p-1">CUARENTENA (2020)</td>
-                                        <td className="p-1"><a href="">Enunciados</a></td>
+                                        <td className="p-1 hover:text-blue-700 hover:transition-all hover:ease-in"><a href="enunciados/CUARENTENA I.pdf">Enunciados</a></td>
                                         <td className="p-1"><a href="">Resultados</a></td>
                                     </tr>
                                     <tr className="h-12">
                                         <td className="p-1">CUARENTENA II (2021)</td>
                                         <td className="p-1">
-                                            <a href="" className="">Día 1</a>
-                                            <a className="block p-2" href="">Día 2</a>
+                                            <a href="enunciados/Cuarentena_2_Día1.pdf" className="p-1 hover:text-blue-700 hover:transition-all hover:ease-in">Día 1</a>
+                                            <a className="block p-1 hover:text-blue-700 hover:transition-all hover:ease-in" href="enunciados/Cuarentena_2_Día2.pdf">Día 2</a>
                                         </td>
                                         <td className="p-1"><a href="">Resultados</a></td>
                                     </tr>
                                     <tr className="h-12 m-3">
                                         <td className="p-1">CUARENTENA III (2021)</td>
-                                        <td className="p-1"><a href="">Enunciados</a></td>
+                                        <td className="p-1 hover:text-blue-700 hover:transition-all hover:ease-in"><a href="enunciados/Cuarentena3.pdf">Enunciados</a></td>
                                         <td className="p-1"><a href="">Resultados</a></td>
                                     </tr>
                                     <tr className="h-12 m-3">
                                         <td className="p-1">Semana de Mórtimer (2022)</td>
-                                        <td className="p-1 hover:text-blue-700 hover:transition-all hover:ease-in"><a href="">Enunciados</a></td>
+                                        <td className="p-1 hover:text-blue-700 hover:transition-all hover:ease-in"><a href="enunciados/Semana_de_Mortimer.pdf">Enunciados</a></td>
                                         <td className="p-1"><a href="">Resultados</a></td>
                                     </tr>
                                     <tr className="h-12 m-3">
                                         <td className="p-1">CUARENTENA IV (2023)</td>
                                         <td className="p-1">
-                                            <a href="" className="">Día 1</a>
-                                            <a className="block p-2" href="">Día 2</a>
+                                            <a href="" className="p-1 hover:text-blue-700 hover:transition-all hover:ease-in">Día 1</a>
+                                            <a className="block p-2 hover:text-blue-700 hover:transition-all hover:ease-in" href="">Día 2</a>
                                         </td>
                                         <td className="p-1"><a href="">Resultados</a></td>
                                     </tr>
