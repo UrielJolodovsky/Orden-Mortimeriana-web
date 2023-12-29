534ff4a3ae2d76ebbc2495f27fa629cd2102dbdd
Uriel Jolodovsky <46960341@est.ort.edu.ar>
2023-12-29 18:00:20 -0300

    Start table results of last "Cuarentena"

--- sitio-cuarentena/src/app/resultados/cuarentena4/page.tsx
+++ sitio-cuarentena/src/app/resultados/cuarentena4/page.tsx
@@ -6,9 +6,24 @@ export default function Home() {
             <div className="p-5 flex justify-center items-center">
                 <h1 className="font-bold">Premiados</h1>
             </div>
-            <div>
+            <div className="flex justify-center items-center">
                 {/* Make table with name, and type of medal */}
-                
+                <div className="border-2 border-black rounded-[15px] bg-white">
+                    <table className="">
+                        <thead>
+                            <tr>
+                                <th className="px-4 py-2">Nombre</th>
+                                <th className="px-4 py-2">Medalla</th>
+                            </tr>
+                        </thead>
+                        <tbody className="text-center">
+                            <tr>
+                                <td className="px-4 py-2">Bruno Ziger</td>
+                                <td className="px-4 py-2 text-yellow-400">Oro</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     )
